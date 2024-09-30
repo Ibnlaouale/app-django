05e91e95fc675abb81a7177f950027c5071f627a
Ibnlaouale <dinilaouale@gmail.com>
2024-09-30 11:00:40 +0100

    creation des model question et choice

--- polls/apps.py
+++ polls/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
 
-class SondagesConfig(AppConfig):
+class PollsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sondages'
+    name = 'polls'
+ 
